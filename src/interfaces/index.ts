7e8981bee9bda49eccddb0a9937acdfd13a96e78
Sargam <sargampoudel100@gmail.com>
2023-11-15 22:21:32 +0530

    Revert "fix: implement interfaces as API responses"
    This reverts commit 6fe7b953b02fac8051a3b51d36820b8b6a8ae15a.

--- src/interfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './posts.interface';
-export * from './user-profile.interface';
--- src/interfaces/posts.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IPost {
-  id: string;
-  title: string;
-  caption: string;
-  imagesUrl: string[];
-  createdAt: Date;
-  updatedAt: Date;
-  authorId: string;
-}

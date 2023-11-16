62846f648adf3db467b43cc1b347275d5a3edbbd
Sargam <sargampoudel100@gmail.com>
2023-11-16 15:38:55 +0530

    BREAKING: switch to vite

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_ENDPOINT,
+  baseURL: import.meta.env.BACKEND_ENDPOINT,
 });
 
 export function setAxiosAuthHeader(token: string) {
